22d86438b03ada592b09902fa1668a436e45b1bb
Jojo <joebert_italia09@yahoo.com>
2024-05-19 01:40:10 +0800

    mongodb connection

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import reactLogo from './assets/react.svg';
+import './App.css'
+
+function App() {
+  const [records, setRecords] = useState([]);
+  const [count, setCount] = useState(0)
+
+  // This method fetches the records from the database.
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5050/record/`);
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      const records = await response.json();
+      setRecords(records);
+    }
+    getRecords();
+    return;
+  }, [records.length]);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5050/record/${id}`, {
+      method: "DELETE",
+    });
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+  
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
